108a189a6bdf293fd99055b126a86a9f155f9b48
katok <katoken629@googlemail.com>
2021-07-14 16:17:37 +0200

    minor changed

--- frontend/src/pages/LetterPage.js
+++ frontend/src/pages/LetterPage.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 
 class LetterPage extends Component {
 
-    render() { 
-        return ( 
+    render() {
+        return (
             <div className="page-container">
                 <h1 className="page-title">Letter</h1>
                 <div className="page-content">
-                    <h4 className="letter-text">ここに手紙を書く</h4>
-               
+
+
                 </div>
             </div>
-         );
+        );
     }
 }
- 
+
 export default LetterPage;
